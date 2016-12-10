08c248e34bb473abd939e011f2dddb11bc0f0b24
Matt Beaney <matt.beaney@pagesuite.co.uk>
2016-12-10 18:17:05 +0000

    Switched to scss

--- app/app.jsx
+++ app/app.jsx
@@ -10,7 +10,7 @@ var Examples = require('Examples');
 
 //Load foundation
 require('style!css!foundation-sites/dist/foundation.min.css');
-require('style!css!applicationStyles')
+require('style!css!sass!applicationStyles')
 
 $(document).foundation();
 
