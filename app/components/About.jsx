457267f4cc8613b23744e654b1f2cc7301aad17f
Matt Beaney <matt.beaney@pagesuite.co.uk>
2016-12-10 15:52:48 +0000

    Deploy challenge

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,10 @@ var React = require('react');
 
 var About = (props) => {
   return (
-    <h2>About Components</h2>
+    <div>
+    <h2>About</h2>
+    <p>Welcome to about.</p>
+    </div>
   )
 };
 
