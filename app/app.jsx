2bced18cd92f8b9759c243ed0ab280943f044fd8
Matt Beaney <matt.beaney@pagesuite.co.uk>
2016-12-10 16:09:43 +0000

    Installed Foundation

--- app/app.jsx
+++ app/app.jsx
@@ -1,10 +1,16 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+//Components
 var Main = require('Main');
 var Weather = require('Weather');
 var About = require('About');
 var Examples = require('Examples');
+
+//Load foundation
+require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
 
 ReactDOM.render(
   <Router history={hashHistory}>
